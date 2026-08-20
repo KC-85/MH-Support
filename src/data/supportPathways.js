41ccdb31d1c99456045c3a443ebc557a573a0de7
Kristian Cross <etamin1985@yahoo.com>
2026-08-20 13:05:53 +0100

    Add support pathway section

---
+++ src/data/supportPathways.js
@@ -0,0 +1,22 @@
+const supportPathways = [
+  {
+    number: '01',
+    title: 'Make sense of it',
+    description:
+      'Understand common experiences through clear, plain-language information.',
+  },
+  {
+    number: '02',
+    title: 'Try something practical',
+    description:
+      'Find grounded ideas that feel realistic, manageable and useful right now.',
+  },
+  {
+    number: '03',
+    title: 'Know where to turn',
+    description:
+      'Explore straightforward routes towards trusted services and further help.',
+  },
+]
+
+export default supportPathways
