9a2675015f80367b3448deed83ed369c7f262e45
Kristian Cross <etamin1985@yahoo.com>
2026-08-20 17:19:29 +0100

    Add vercel routes, remove github pages script, remove obsolete 404.html

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command, isPreview }) => ({
-  base: command === 'build' || isPreview ? '/MH-Support/' : '/',
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-}))
+})
